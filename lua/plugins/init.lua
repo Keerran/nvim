194f3826b4ff83783c52ad5d6339e3da5368ebf5
Keerran <kierankumar@gmail.com>
2023-11-09 20:34:08 +0000

    feat: turn off project.nvim manual mode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -53,7 +53,7 @@ return {
     {
         "ahmedkhalf/project.nvim",
         main = "project_nvim",
-        opts = {},
+        opts = { manual_mode = true },
         init = function ()
             require('telescope').load_extension('projects')
             vim.keymap.set("n", "<leader>fp", require'telescope'.extensions.projects.projects)
