f1344f49c7ab917bd6cd45656e99d0796f94c43c
Keerran <kierankumar@gmail.com>
2023-11-10 01:04:34 +0000

    refactor: add comments to options

--- lua/options.lua
+++ lua/options.lua
@@ -1,23 +1,32 @@
 vim.g.mapleader = "\\"
 
+-- allow mouse events
 vim.opt.mousemoveevent = true
 
+-- 24-bit colors
 vim.opt.termguicolors = true
 
+-- no line wrap
 vim.opt.wrap = false
+
+-- lin numbers
 vim.opt.number = true
 
+-- incremental search
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
+-- 4 width soft tabs
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
+-- ignore case in commands and searches
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+-- unix-style autocomplete
 vim.opt.wildmode =  "longest,full"
 
 -- highlight yanked text for 200ms using the "Cursor" highlight group
