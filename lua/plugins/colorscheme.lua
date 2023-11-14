061e03d911f4b5b7e419610a668d3961eb9f5715
Keerran <kierankumar@gmail.com>
2023-11-14 19:17:28 +0000

    feat: add more colorschemes and switch to catppuccin transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,42 @@
 return {
-    "sainnhe/everforest",
+    {
+        "sainnhe/everforest",
+        lazy = true,
+    },
     {
         "sainnhe/gruvbox-material",
         priority = 1000,
         config = function ()
-            vim.opt.background = "dark"
             vim.g.gruvbox_material_background = "soft"
-            vim.cmd.colorscheme("gruvbox-material")
         end,
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = true,
+        opts = {
+            transparent_background = true,
+            integrations = {
+                navic = {
+                    enabled = true,
+                }
+            }
+        },
+        init = function ()
+            vim.opt.background = "dark"
+            vim.cmd.colorscheme("catppuccin")
+        end
+    },
+    {
+        "marko-cerovac/material.nvim",
+        lazy = true,
+    },
+    {
+        "sainnhe/sonokai",
+        lazy = true,
+    },
+    {
+        "tiagovla/tokyodark.nvim",
+        lazy = true,
     }
 }
