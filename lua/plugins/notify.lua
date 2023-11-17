1800c41d8f8c3ccf8a74a9f54e4f0bb02303cc00
Keerran <kierankumar@gmail.com>
2023-11-17 19:00:55 +0000

    fix: give notify a background colour for transparency

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,6 +1,9 @@
 return {
     "rcarriga/nvim-notify",
-    config = function ()
+    opts = {
+        background_colour = "#000000",
+    },
+    init = function ()
         vim.notify = require("notify")
     end
 }
