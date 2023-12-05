8504b8552fe774d2c98be36b00ebd31fd7b3a6cc
Keerran <kierankumar@gmail.com>
2023-12-05 21:29:48 +0000

    feat: add nvim-treesitter-textobjects

--- lua/keerran/plugins/treesitter.lua
+++ lua/keerran/plugins/treesitter.lua
@@ -3,7 +3,8 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         dependencies = {
-            "windwp/nvim-ts-autotag"
+            "windwp/nvim-ts-autotag",
+            "nvim-treesitter/nvim-treesitter-textobjects",
         },
         config = function()
             require("nvim-treesitter.configs").setup {
@@ -15,9 +16,29 @@ return {
                 },
                 autotag = {
                     enable = true
+                },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["as"] = { query = "@scope", query_group = "locals" },
+                        }
+                    },
+                    move = {
+                        enable = true,
+                        goto_next_start = {
+                            ["]f"] = "@function.outer",
+                            ["]s"] = { query = "@scope", query_group = "locals" },
+                        },
+                        goto_previous_start = {
+                            ["[f"] = "@function.outer",
+                            ["[s"] = { query = "@scope", query_group = "locals" },
+                        }
+                    }
                 }
             }
         end
     },
-    -- { "nvim-treesitter/nvim-treesitter-context" },
 }
