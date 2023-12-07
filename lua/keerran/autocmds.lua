07c029d104e7f0b95a393758d52e041b95d9e534
Keerran <kierankumar@gmail.com>
2023-12-07 00:25:55 +0000

    refactor: use lua api for autocmds

--- lua/keerran/autocmds.lua
+++ lua/keerran/autocmds.lua
@@ -1,7 +1,8 @@
 -- highlight yanked text for 200ms using the "Cursor" highlight group
-vim.cmd [[
-augroup highlight_yank
-autocmd!
-au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Cursor", timeout=100})
-augroup END
-]]
+vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = "YankHighlight",
+    callback = function()
+        vim.highlight.on_yank({higroup="Cursor", timeout=100})
+    end
+})
