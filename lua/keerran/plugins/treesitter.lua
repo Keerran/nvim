694f2de7160fe4d42c89b3932d4219d1ecbb1d0b
Keerran <kierankumar@gmail.com>
2024-01-04 00:10:40 +0000

    feat(treesitter): add incremental selection

--- lua/keerran/plugins/treesitter.lua
+++ lua/keerran/plugins/treesitter.lua
@@ -17,6 +17,15 @@ return {
                 autotag = {
                     enable = true
                 },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<CR>",
+                        node_incremental = "<CR>",
+                        scope_incremental = "<S-CR>",
+                        node_decremental = "<BS>",
+                    }
+                },
                 textobjects = {
                     select = {
                         enable = true,
