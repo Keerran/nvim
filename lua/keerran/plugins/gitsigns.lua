aab2ea3daa5071efaecbb6ff02793df4a787851c
Keerran <kierankumar@gmail.com>
2025-08-22 15:48:19 +0100

    feat: git support

---
+++ lua/keerran/plugins/gitsigns.lua
@@ -0,0 +1,50 @@
+return {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            on_attach = function (bufnr)
+                if vim.api.nvim_buf_get_name(bufnr):match('%.ipynb$') then
+                  -- Do not attach for .ipynb file, since these are converted
+                  -- with jupytext.nvim
+                  return false
+                end
+
+                local gs = package.loaded.gitsigns
+
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                -- Navigation
+                map('n', ']c', function()
+                    if vim.wo.diff then return ']c' end
+                    vim.schedule(function() gs.next_hunk() end)
+                    return '<Ignore>'
+                end, {expr=true, desc = "Next hunk"})
+
+                map('n', '[c', function()
+                    if vim.wo.diff then return '[c' end
+                    vim.schedule(function() gs.prev_hunk() end)
+                    return '<Ignore>'
+                end, {expr=true, desc = "Prev hunk"})
+
+                -- Actions
+                map_group({ 'n', 'v' }, "<leader>h", "Git staging")
+                map('n', '<leader>hs', gs.stage_hunk, { desc = "Stage hunk" })
+                map('n', '<leader>hr', gs.reset_hunk, { desc = "Reset hunk" })
+                map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "Stage hunk" })
+                map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "Reset hunk" })
+                map('n', '<leader>hS', gs.stage_buffer, { desc = "Stage buffer" })
+                map('n', '<leader>hu', gs.undo_stage_hunk, { desc = "Unstage hunk" })
+                map('n', '<leader>hR', gs.reset_buffer, { desc = "Reset buffer" })
+                map('n', '<leader>hp', gs.preview_hunk, { desc = "Preview hunk" })
+                map('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = "Blame line" })
+                map('n', '<leader>hd', gs.diffthis, { desc = "Diff" })
+                map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = "Diff file" })
+            end
+        },
+        init = function ()
+            vim.opt.signcolumn = "yes"
+        end
+}
