50f86cb461243db280c99688be274b7592022d0e
Keerran <kierankumar@gmail.com>
2025-08-22 15:45:03 +0100

    feat: lualine

---
+++ lua/keerran/plugins/lualine.lua
@@ -0,0 +1,22 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "SmiteshP/nvim-navic",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        options = {
+            theme = "auto",
+            globalstatus = true,
+        },
+        winbar = {
+            lualine_c = {
+                {
+                    "navic",
+                    color_correction = nil,
+                    navic_opts = nil
+                }
+            }
+        }
+    },
+}
