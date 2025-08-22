eb21eec12b36f64fff06c191ec6ed93de26553eb
Keerran <kierankumar@gmail.com>
2025-08-22 20:46:43 +0100

    feat: abbreviations

---
+++ lua/keerran/config/abbreviations.lua
@@ -0,0 +1,23 @@
+local abbreviations = {
+    W = "w",
+    Q = "q",
+    E = "e",
+    Qa = "qa",
+    Wq = "wq",
+    Wqa = "wqa",
+    Norg = "Neorg",
+    norg = "Neorg",
+    git = "Git",
+    lazy = "Lazy",
+    mason = "Mason",
+}
+
+for lhs, rhs in pairs(abbreviations) do
+    vim.keymap.set("ca", lhs, function ()
+        if vim.fn.getcmdtype() == ":" and vim.fn.getcmdline() == lhs then
+            return rhs
+        else
+            return lhs
+        end
+    end, { noremap = true, expr = true })
+end
