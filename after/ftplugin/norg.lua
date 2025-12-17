bbda0adf41849c6b5d004fd1d2ed82bed00ad601
Keerran <kierankumar@gmail.com>
2025-12-17 16:13:39 +0000

    feat: neorg

---
+++ after/ftplugin/norg.lua
@@ -0,0 +1,8 @@
+vim.opt_local.conceallevel = 3
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+
+vim.keymap.set("x", "h", "gh")
+vim.keymap.set("x", "j", "gj")
+vim.keymap.set("x", "k", "gk")
+vim.keymap.set("x", "l", "gl")
---
+++ lua/keerran/plugins/neorg.lua
@@ -0,0 +1,30 @@
+return {
+    "nvim-neorg/neorg",
+    -- enabled = false,
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    opts = {
+        load = {
+            ["core.defaults"] = {}, -- Loads default behaviour
+            ["core.concealer"] = {}, -- Adds pretty icons to your documents
+            ["core.completion"] = {
+                config = {
+                    engine = "nvim-cmp",
+                }
+            },
+            ["core.summary"] = {},
+            ["core.promo"] = {},
+            ["core.keybinds"] = {},
+            ["core.esupports.metagen"] = {},
+            ["core.dirman"] = { -- Manages Neorg workspaces
+                config = {
+                    workspaces = {
+                        notes = "/mnt/shared/notes",
+                        work = "/mnt/shared/Work/notes"
+                    },
+                },
+            },
+            ["core.integrations.nvim-cmp"] = {},
+        }
+    },
+}
