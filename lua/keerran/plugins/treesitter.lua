a0cc76c21a2f98ea79c1e6cb7bd2fdb0fe05f698
Keerran <kierankumar@gmail.com>
2025-12-17 16:14:00 +0000

    feat: nvim-ts-autotag

--- lua/keerran/plugins/treesitter.lua
+++ lua/keerran/plugins/treesitter.lua
@@ -35,5 +35,12 @@ return {
                 }
             }
         end,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
+        opts = {}
     }
 }
