5733051d933e526d78a9de648449a4a03f4a5c11
Keerran <kierankumar@gmail.com>
2025-12-17 16:18:54 +0000

    fix: navic integration for catppuccin

--- lua/keerran/plugins/colorschemes.lua
+++ lua/keerran/plugins/colorschemes.lua
@@ -5,6 +5,11 @@ return {
         lazy = true,
         opts = {
             transparent_background = true,
+            integrations = {
+                navic = {
+                    enabled = true,
+                }
+            }
         },
         init = function ()
             vim.opt.background = "dark"
