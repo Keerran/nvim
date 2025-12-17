ec4c67624f1a969e55248a71016df0143046e8e2
Keerran <kierankumar@gmail.com>
2025-12-17 16:19:54 +0000

    fix: transparent floating panels

--- lua/keerran/plugins/colorschemes.lua
+++ lua/keerran/plugins/colorschemes.lua
@@ -14,6 +14,10 @@ return {
         init = function ()
             vim.opt.background = "dark"
             vim.cmd.colorscheme("catppuccin")
+            local groups = { "NormalFloat", "FloatBorder", "FloatTitle", "FloatFooter" }
+            for _, group in ipairs(groups) do
+                vim.cmd([[hi ]] .. group .. [[ guibg=NONE]])
+            end
         end
     },
 }
