dc2c2a3a0a25263a3c68a8c96fb6414d48d5252a
Keerran <kierankumar@gmail.com>
2025-12-22 13:56:29 +0000

    fix: use mode argument in `map_group`

--- lua/keerran/globals.lua
+++ lua/keerran/globals.lua
@@ -4,6 +4,6 @@
 function _G.map_group(mode, key, name)
     local ok, wk = pcall(require, "which-key")
     if ok then
-        wk.add({{ key, group = name }})
+        wk.add({{ key, mode = mode, group = name }})
     end
 end
