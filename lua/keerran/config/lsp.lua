96ed4a1371657aaa365e184ed2ceaa32a33e3f8e
Keerran <kierankumar@gmail.com>
2026-07-14 14:28:02 +0100

    feat: add more LSPs

--- lua/keerran/config/lsp.lua
+++ lua/keerran/config/lsp.lua
@@ -2,7 +2,14 @@ local lsps = {
     "lua_ls",
     "ty",
     "rust_analyzer",
+    "gdscript",
     "nil_ls",
+    "ruff",
+    "terraformls",
+    "lemminx",
+    "jsonls",
+    "ts_ls",
+    "clangd"
 }
 
 for _, lsp in ipairs(lsps) do
